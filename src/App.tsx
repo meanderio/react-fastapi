31a21e5ac510ea0ab986895c8a7d8506e847540a
Aaron Cahn <acahn22@gmail.com>
2025-11-05 10:30:08 -0600

    update: adding all src dir files and other config

---
+++ src/App.tsx
@@ -0,0 +1,69 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import api from './api'
+
+interface Purchase {
+  location: string;
+  amount: number;
+  purchaser_id: number;
+}
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [purchase, setPurchase] = useState<Purchase>()
+
+
+  const fetchPurchase = async () => {
+    const response = await api.get('/api/purchases/2');
+    console.log(response.data);
+    setPurchase(response.data);
+  };
+
+  //useEffect(() => {
+  //  fetchPurchase();
+  //}, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        {/*<button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        */}
+        <button onClick={() => fetchPurchase()}>
+          Get Purchase
+        </button>
+        <div>
+          { purchase 
+            ? (
+              <div>
+                <p>purchase location: {purchase.location}</p>
+                <p>purchase amount: {purchase.amount}</p>
+              </div>
+            )
+            : (<p> no purchase loaded </p>)
+          }
+        </div>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App;
